083b2a7fd5e787e010a6328f6d01930a3b27cbf3
abhiataero <abhiataero@gmail.com>
2013-12-05 02:05:02 -0800

    Update MDCS.py
    Updated Version

--- scripts/MDCS.py
+++ scripts/MDCS.py
@@ -1,7 +1,7 @@
 #-------------------------------------------------------------------------------
 # Name  	    	: MDCS.py
 # ArcGIS Version	: ArcGIS 10.1 sp1
-# Script Version	: 20130801
+# Script Version	: 20131205
 # Name of Company 	: Environmental System Research Institute
 # Author        	: ESRI raster solution team
 # Purpose 	    	: This is the main program entry point to MDCS.
