3f1be2f82ba0003d2e52297b6f5e6b4f28923023
abhiataero <abhiataero@gmail.com>
2013-12-05 02:04:27 -0800

    Update logger.py
    Updated Version

--- scripts/SolutionsLog/logger.py
+++ scripts/SolutionsLog/logger.py
@@ -1,7 +1,7 @@
 #-------------------------------------------------------------------------------
 # Name  	    	: Logger.py
 # ArcGIS Version	: ArcGIS 10.1 sp1
-# Script Version	: 20130801
+# Script Version	: 20131205
 # Name of Company 	: Environmental System Research Institute
 # Author        	: ESRI raster solution team
 # Purpose 	    	: Class to log status from components to log files.
