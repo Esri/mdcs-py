ee61d03ddadf1cd69d6d16401e9948a4f2ce5070
abhiataero <abhiataero@gmail.com>
2013-12-05 02:05:27 -0800

    Update MDCS_UC.py
    Updated version

--- scripts/MDCS_UC.py
+++ scripts/MDCS_UC.py
@@ -1,7 +1,7 @@
 #-------------------------------------------------------------------------------
 # Name  	    	: MDCS_UC.py
 # ArcGIS Version	: ArcGIS 10.1 sp1
-# Script Version	: 20131017
+# Script Version	: 20131205
 # Name of Company 	: Environmental System Research Institute
 # Author        	: ESRI raster solution team
 # Purpose 	    	: A class to define all user defined functions to extend
