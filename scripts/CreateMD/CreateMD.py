f488499b29f2691fd5b5b536147118e07f3a324a
Chamlika Wickramasinghe <cwickramasinghe@esri.com>
2016-07-25 13:18:12 +0400

    Update CreateMD.py
    Fix> To omit optional values in the input MDCS parameter file when Creating a new MD.

--- scripts/CreateMD/CreateMD.py
+++ scripts/CreateMD/CreateMD.py
@@ -14,7 +14,7 @@
 #------------------------------------------------------------------------------
 # Name: CreateMD.py
 # Description: Creates source mosaic datasets.
-# Version: 20140417
+# Version: 20160721
 # Requirements: ArcGIS 10.1 SP1
 # Author: Esri Imagery Workflows team
 #------------------------------------------------------------------------------
@@ -27,21 +27,16 @@ import Base
 
 class CreateMD(Base.Base):
     def __init__(self, base):
-
         self.srs = ''
         self.pixel_type = ''
         self.product_definition = ''
         self.num_bands = ''
         self.product_band_definitions = ''
-
         self.setLog(base.m_log)
         self.m_base = base
-
-
     def createGeodataBase(self):
         if (self.m_base.m_IsSDE == True):       # MDCS doesn't create new SDE connections and assumes .SDE connection passed on exists @ the server
             return True                         # to create new Mosaic Datasets.
-
         # create output workspace if missing.
         if (os.path.exists(self.m_base.m_workspace) == False):
             try:
@@ -50,7 +45,6 @@ class CreateMD(Base.Base):
                 self.log("Failed to create folder: " + self.m_base.m_workspace, self.const_critical_text)
                 self.log(arcpy.GetMessages(), self.const_critical_text)
                 return False
-
         # create Geodatabase
         try:
             self.log('Creating Geodatabase: ({})'.format(self.m_base.m_geoPath), self.const_general_text)
@@ -62,46 +56,43 @@ class CreateMD(Base.Base):
         except:
             return False
 
-
     def createMD(self):
         self.log("Creating source mosaic datasets:", self.const_general_text)
-
         try:
             mdPath = os.path.join(self.m_base.m_geoPath, self.m_base.m_mdName)
             if not arcpy.Exists(mdPath):
                 self.log("\t" + self.m_base.m_mdName, self.const_general_text)
                 arcpy.CreateMosaicDataset_management(self.m_base.m_geoPath,self.m_base.m_mdName,self.srs,self.num_bands,self.pixel_type,self.product_definition,self.product_band_definitions)
-
         except:
             self.log("Failed!", self.const_critical_text)
             self.log(arcpy.GetMessages(), self.const_critical_text)
             return False
-
         return True
-
 
     def init(self, config):
         Nodelist = self.m_base.m_doc.getElementsByTagName("MosaicDataset")
         if (Nodelist.length == 0):
-            self.log("\nError: MosaicDatasets node not found! Invalid schema.", self.const_critical_text)
+            self.log("\nErr. MosaicDatasets node not found! Invalid schema.", self.const_critical_text)
             return False
-
         try:
             for node in Nodelist[0].childNodes:
                   node =  node.nextSibling
                   if (node != None and node.nodeType == minidom.Node.ELEMENT_NODE):
-                      if(node.nodeName == 'SRS'):
+                      if(node.nodeName == 'SRS'):   # required arg
                             self.srs = node.firstChild.nodeValue
-                      elif(node.nodeName == 'pixel_type'):
-                            self.pixel_type = node.firstChild.nodeValue
-                      elif(node.nodeName == 'num_bands'):
-                            self.num_bands = node.firstChild.nodeValue
-                      elif(node.nodeName == 'product_definition'):
-                            self.product_definition = node.firstChild.nodeValue
-                      elif(node.nodeName == 'product_band_definitions'):
-                            self.product_band_definitions = node.firstChild.nodeValue
+                      elif(node.nodeName == 'pixel_type'):  # optional arg
+                            if (node.hasChildNodes()):
+                                self.pixel_type = node.firstChild.nodeValue
+                      elif(node.nodeName == 'num_bands'):   # optional arg
+                            if (node.hasChildNodes()):
+                                self.num_bands = node.firstChild.nodeValue
+                      elif(node.nodeName == 'product_definition'):  # optional arg
+                            if (node.hasChildNodes()):
+                                self.product_definition = node.firstChild.nodeValue
+                      elif(node.nodeName == 'product_band_definitions'):    # optional arg
+                            if (node.hasChildNodes()):
+                                self.product_band_definitions = node.firstChild.nodeValue
         except:
-            self.log("\nError: reading MosaicDataset nodes.", self.const_critical_text)
+            self.log("\nErr. Reading MosaicDataset nodes.", self.const_critical_text)
             return False
-
         return True
