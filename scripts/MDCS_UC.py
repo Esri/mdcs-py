814fb46dc007b5ee9cbb49a07d0c4242c4e37986
Chamlika Wickramasinghe <cwickramasinghe@esri.com>
2017-04-25 11:48:30 +0400

    Update MDCS_UC.py
    Enhancements> 
    1. -s: can be repeated at the command line to add multiple source paths.
    2. Formatted with pep8  standards.
    3. Support added to access the 'sourcePath' and the 'base' object within user functions using the data argument.

--- scripts/MDCS_UC.py
+++ scripts/MDCS_UC.py
@@ -14,84 +14,62 @@
 #------------------------------------------------------------------------------
 # Name: MDCS_UC.py
 # Description: A class to implement all user functions or to extend the built in MDCS functions/commands chain.
-# Version: 20140417
+# Version: 20170425
 # Requirements: ArcGIS 10.1 SP1
 # Author: Esri Imagery Workflows team
 #------------------------------------------------------------------------------
 #!/usr/bin/env python
-
 import os
 import sys
 import arcpy
 
-import Base
 
 class UserCode:
-    def sample00(self, data):
 
-        base = Base.Base()          # using Base class for its XML specific common functions. (getXMLXPathValue, getXMLNodeValue, getXMLNode)
+    def sample00(self, data):
+        base = data['base']         # using Base class for its XML specific common functions. (getXMLXPathValue, getXMLNodeValue, getXMLNode)
         xmlDOM = data['mdcs']       # access to MDCS config file
         command_used = base.getXMLNodeValue(xmlDOM, 'Command')
-
         workspace = data['workspace']
         md = data['mosaicdataset']
-
         log = data['log']
         log.Message('%s\\%s' % (workspace, md), 0)
-
         return True
-
-
 
     def sample01(self, data):
-
-        log = data['log']
+        log = data['log']           # How to use logging within the user function.
         log.Message('hello world', 0)
-
         return True
 
-
     def customCV(self, data):
-
         workspace = data['workspace']
         md = data['mosaicdataset']
-
         ds = os.path.join(workspace, md)
         ds_cursor = arcpy.UpdateCursor(ds)
-        if (ds_cursor != None):
+        if (ds_cursor is not None):
             print ('Calculating values..')
             row = ds_cursor.next()
-            while(row != None):
+            while(row is not None):
                 row.setValue('MinPS', 0)
                 row.setValue('MaxPS', 300)
-
                 WRS_Path = row.getValue('WRS_Path')
                 WRS_Row = row.getValue('WRS_Row')
-
-                if (WRS_Path != None and
-                    WRS_Row != None):
-                    PR = (WRS_Path*1000)+WRS_Row
+                if (WRS_Path is not None and
+                        WRS_Row is not None):
+                    PR = (WRS_Path * 1000) + WRS_Row
                     row.setValue('PR', PR)
-
-
                 AcquisitionData = row.getValue('AcquisitionDate')
-                if (AcquisitionData != None):
+                if (AcquisitionData is not None):
                     AcquisitionData = str(AcquisitionData).replace('-', '/')
                     day = int(AcquisitionData.split()[0].split('/')[1])
                     row.setValue('Month', day)
-
-
                 grp_name = row.getValue('GroupName')
-                if (grp_name != None):
+                if (grp_name is not None):
                     CMAX_INDEX = 16
                     if (len(grp_name) >= CMAX_INDEX):
                         row.setValue('DayOfYear', int(grp_name[13:CMAX_INDEX]))
                         row.setValue('Name', grp_name.split('_')[0] + '_' + row.getValue('Tag'))
-
                 ds_cursor.updateRow(row)
                 row = ds_cursor.next()
-
             del ds_cursor
-
-
-        return True
+        return True 
