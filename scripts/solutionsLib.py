e38a2717ca4f6b0fb44190cf115a7be0ec3326e3
bikesh bade <36373140+bikesbade@users.noreply.github.com>
2022-10-26 11:25:36 +0400

    Update solutionsLib.py

--- scripts/solutionsLib.py
+++ scripts/solutionsLib.py
@@ -1064,11 +1064,11 @@ class Solutions(Base.Base):
                     self.getProcessInfoValue(processKey, 'max_cached_scale', index))
                 if os.path.isfile(tileSchemeMtc):
                     cachepath = os.path.join(self.getProcessInfoValue(processKey, 'in_cache_location', index), self.getProcessInfoValue(processKey, 'in_cache_name', index))
-                    lodNodesList = returnLevelDetails(os.path.join(cachepath, 'conf.xml'))
+                    lodNodesList = self.returnLevelDetails(os.path.join(cachepath, 'conf.xml'))
                     lodNodesList = sorted(lodNodesList, key=lambda k: int(k['level']))
                     maxLODNode = lodNodesList[-1]
                     maxScale = maxLODNode['scale']
-                    modifyConfProperties(os.path.join(cachepath, 'conf.properties'), maxScale)
+                    self.modifyConfProperties(os.path.join(cachepath, 'conf.properties'), maxScale)
                 return True
             except BaseException:
                 self.log(arcpy.GetMessages(), self.m_log.const_critical_text)
@@ -1855,3 +1855,28 @@ class Solutions(Base.Base):
                  is_user_cmd == True)):
                 break
         return cmdResults
+
+    def returnLevelDetails(tilingSchema):
+        doc = minidom.parse(tilingSchema)
+        lodNodesList = []
+        lodNodes = doc.getElementsByTagName('LODInfo')
+        for lodNode in lodNodes:
+            level = lodNode.getElementsByTagName('LevelID')[0].firstChild.data
+            levelDict = {
+                            "level": level,
+                            "scale": lodNode.getElementsByTagName('Scale')[0].firstChild.data,
+                            "resolution": lodNode.getElementsByTagName('Resolution')[0].firstChild.data
+                        }
+            lodNodesList.append(levelDict)
+        return lodNodesList
+
+    def modifyConfProperties(properties, maxScale):
+        doc = minidom.parse(properties)
+        keys = doc.getElementsByTagName('Key')
+        values = doc.getElementsByTagName('Value')
+        key = [k for k in keys if k.childNodes[0].nodeValue == 'MaxScale'][0]
+        value = [v for v in values if v.parentNode.isSameNode(key.parentNode)][0]
+        value.childNodes[0].replaceWholeText(maxScale)
+        modifiedXML = doc.toxml()
+        with open(properties, 'w') as xmlHandler:
+            xmlHandler.write(modifiedXML)
